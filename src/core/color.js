53d23b00018c98167db03eb31ce6fca0db0001c4
czy941030 <czy941030@gmail.com>
2015-10-10 11:01:07 +0800

    Modify Canvas Modify color (r, g, b) -> (r, g, b, a)

--- src/core/color.js
+++ src/core/color.js
@@ -4,13 +4,14 @@
  */
 
 class Color {
-    constructor (r, g, b) {
+    constructor (r, g, b, a) {
         this.r = r;
         this.g = g;
         this.b = b;
+        this.a = a;
     }
 }
 
-export var white = new Color(255, 255, 255);
-export var black = new Color(0, 0, 0);
+export var white = new Color(255, 255, 255, 255);
+export var black = new Color(0, 0, 0, 255);
 export default Color;
