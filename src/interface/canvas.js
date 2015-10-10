e6d90022395798026b7fbaff061598dce13b7b5c
quietshu <ds303077135@gmail.com>
2015-10-10 22:37:03 +0800

    Canvas modification

--- src/interface/canvas.js
+++ src/interface/canvas.js
@@ -5,19 +5,21 @@
 
 class Canvas {
 
-    constructor (canvas) {
-        this.cvs = canvas;
+    constructor(canvas) {
+        this.cvs     = canvas;
         this.context = canvas.getContext("2d");
-        this.width = canvas.width;
-        this.height = canvas.height;
+        this.width   = canvas.width;
+        this.height  = canvas.height;
         this.imgData = this.context.getImageData(0, 0, canvas.width, canvas.height);
     }
 
-    set(x, y, color) {
-        this.imgData.data[(y * this.width + x) * 4] = color.r;
-        this.imgData.data[(y * this.width + x) * 4 + 1] = color.g;
-        this.imgData.data[(y * this.width + x) * 4 + 2] = color.b;
-        this.imgData.data[(y * this.width + x) * 4 + 3] = color.a;
+    setPoint(x, y, color) {
+        let index = (y * this.width + x) * 4;
+
+        this.imgData.data[index]     = color.r;
+        this.imgData.data[index + 1] = color.g;
+        this.imgData.data[index + 2] = color.b;
+        this.imgData.data[index + 3] = color.a;
     }
 
     updateCanvas() {
